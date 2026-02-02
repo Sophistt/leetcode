9d55dd71b136f0a0515c37cf19f474b850be4ac9
Sophistt <wang.canye@gmail.com>
2026-02-03 00:24:28 +0800

    增加189. 轮转数组题解

---
+++ java/src/leetcode/Solution_189.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+    给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+示例 1:
+    输入: nums = [1,2,3,4,5,6,7], k = 3
+    输出: [5,6,7,1,2,3,4]
+    解释:
+    向右轮转 1 步: [7,1,2,3,4,5,6]
+    向右轮转 2 步: [6,7,1,2,3,4,5]
+    向右轮转 3 步: [5,6,7,1,2,3,4]
+ 示例 2:
+    输入：nums = [-1,-100,3,99], k = 2
+    输出：[3,99,-1,-100]
+    解释:
+    向右轮转 1 步: [99,-1,-100,3]
+    向右轮转 2 步: [3,99,-1,-100]
+ **/
+
+public class Solution_189 {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reserve(nums, 0, nums.length - 1);
+        reserve(nums, 0, k - 1);
+        reserve(nums, k, nums.length - 1);
+    }
+
+    // 双指针法 reverse 数组
+    public void reserve(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    // debug
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        Solution_189 s = new Solution_189();
+        s.rotate(arr, 7);
+    }
+}
