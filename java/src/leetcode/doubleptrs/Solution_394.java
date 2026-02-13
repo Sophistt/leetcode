a95bddb331500b08eac720a3ac715eec0bc757d1
Sophistt <wang.canye@gmail.com>
2026-02-13 20:10:29 +0800

    添加 392. 判断子序列 题解

---
+++ java/src/leetcode/doubleptrs/Solution_394.java
@@ -0,0 +1,41 @@
+package leetcode.doubleptrs;
+
+//给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+//
+//字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+//
+//进阶：
+//
+//如果有大量输入的 S，称作 S1, S2, ... , Sk 其中 k >= 10亿，你需要依次检查它们是否为 T 的子序列。在这种情况下，你会怎样改变代码？
+//
+//致谢：
+//
+//特别感谢 @pbrother 添加此问题并且创建所有测试用例。
+//
+//
+//
+//示例 1：
+//
+//输入：s = "abc", t = "ahbgdc"
+//输出：true
+//示例 2：
+//
+//输入：s = "axc", t = "ahbgdc"
+//输出：false
+
+
+public class Solution_394 {
+    // 双指针，当 t 跑到末尾的时候，如果 s 已经到末尾就代表已经得到了结果
+    public boolean isSubsequence(String s, String t) {
+        int sp = 0, tp = 0;
+        while (tp < t.length() && sp < s.length()) {
+            if (s.charAt(sp) == t.charAt(tp)) {
+                tp++;
+                sp++;
+            } else {
+                tp++;
+            }
+        }
+        return sp == s.length();
+    }
+}
